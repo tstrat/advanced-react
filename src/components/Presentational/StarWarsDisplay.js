cc2147607374bc7059a47080c588abe0a201a341
joshborup <joshborup@gmail.com>
2018-11-12 01:49:36 -0700

    ready for class

--- src/components/Presentational/StarWarsDisplay.js
+++ src/components/Presentational/StarWarsDisplay.js
@@ -2,17 +2,7 @@ import React, { Component } from "react";
 
 class StarWars extends Component {
 	render() {
-		console.log(this.props);
-		const characterList = this.props.characters.map((character) => {
-			return (
-				<div className="star-wars-char">
-					<span>name: {character.name}</span>
-					<span>height: {character.height}</span>
-					<span>mass: {character.mass}</span>
-				</div>
-			);
-		});
-		return <div>{characterList}</div>;
+		return <div>Star Wars Display</div>;
 	}
 }
 export default StarWars;
