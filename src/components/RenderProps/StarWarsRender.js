d9e0b4ab8e0c7c0fc843975dd9b22077717641fd
joshborup <joshborup@gmail.com>
2018-11-12 00:55:22 -0700

    updated render in starwars fetcher

--- src/components/RenderProps/StarWarsRender.js
+++ src/components/RenderProps/StarWarsRender.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 
 class StarWars extends Component {
 	render() {
-		const { name, height, mass } = this.props.data;
+		const { name, height, mass, eye_color } = this.props.data;
 		return (
 			<div className="star-wars-char">
 				<span>name: {name}</span>
 				<span>height: {height}</span>
 				<span>mass: {mass}</span>
+				<span>eye_color: {eye_color}</span>
 			</div>
 		);
 	}
